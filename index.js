0c03b6fcdba6be844bb9e790be43b318c8372c76
Kevin Britz <kevin@totient.capital>
2019-09-21 14:49:00 -0700

    Add mainnet contract

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const { BigNumber } = require('bignumber.js');
 let config = {
   providerPrivateKey: "0xdce1443bd2ef0c2631adc1c67e5c93f13dc23a41c18b536effbbdcbcdb96fb65",  // First local node PK for testing
   freyjaContractAddresses: new Map([[
-    39, "0x08575118e7fe21e31e25791b3ef5507518ce67dd"
+    39, "0x08575118e7fe21e31e25791b3ef5507518ce67dd",
+    74, "0x83e280bbdbe001e35a5b7eaf9c48af7c7c07362a"
   ]]),
   relayerFeeAddress: "0x75e8ea1abd794bf1ecb2a895ce4d0f2388abb62d",
   relayerFee: new BigNumber("0.03"),
@@ -26,9 +27,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-
 
 app.post('/swap', (req, res) => {
   const {
